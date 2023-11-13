7fd181fab7dde5cf159bbe6bdaea62c88b70cafa
QashDev-7 <149897639+QashDev-7@users.noreply.github.com>
2023-11-13 23:58:43 +0500

    new cahnges

--- index.js
+++ index.js
@@ -1,180 +1,218 @@
-
 import validate from "./utlils/validate.js";
-import {checkPaymentSection,checkContactInfoSection,checkPersonalInfoSection} from "./utlils/checkReview.js"
+import {
+  checkPaymentSection,
+  checkContactInfoSection,
+  checkPersonalInfoSection,
+} from "./utlils/checkReview.js";
 
 const next_btn = document.getElementsByClassName("next-btn");
 for (let btn of next_btn) {
-    btn.addEventListener("click", handleNextButtonClick);
+  btn.addEventListener("click", handleNextButtonClick);
 }
 
 validate();
 
 function checkDisclosure() {
-    if (!document.getElementById("Disclosure").checked) {
-        throw new Error("Please accept the terms");
-    }
+  if (!document.getElementById("Disclosure").checked) {
+    throw new Error("diclosre-terms");
+  }
 }
 
 // New function to check if at least one eligibility option is selected
 function validateEligibility() {
-    var eligibilityRadioButtons = document.querySelectorAll(
-        'input[name="eligibility"]'
-    );
-    for (var i = 0; i < eligibilityRadioButtons.length; i++) {
-        if (eligibilityRadioButtons[i].checked) {
-        return true; // At least one option is selected
-        }
+  var eligibilityRadioButtons = document.querySelectorAll(
+    'input[name="eligibility"]'
+  );
+  for (var i = 0; i < eligibilityRadioButtons.length; i++) {
+    if (eligibilityRadioButtons[i].checked) {
+      return true; // At least one option is selected
     }
-    return false; // No option is selected
+  }
+  return false; // No option is selected
 }
-    
+
 function NextButtonClick(event) {
-    if (!validateEligibility()) {
-        alert("Please select an eligibility option.");
-    } else {
-        handleNextButtonClick(event);
-    } 
+  if (!validateEligibility()) {
+    document.getElementById("elgibility-error").innerText =
+      "Please select an eligibility option.";
+  } else {
+    document.getElementById("elgibility-error").innerText = "";
+    handleNextButtonClick(event);
+  }
 }
 
+function cardAddress() {
+  if (document.getElementById("shipping-address").checked === true) {
+    document.getElementById("other-card-address").style.display = "none";
+    let content = `${document.getElementById("address").value}, ${
+      document.getElementById("aptFloorOther").value
+    },  ${document.getElementById("city").value}, ${
+      document.getElementById("state").value
+    }, ${document.getElementById("zipCode").value}`;
 
-
-function cardAddress(){
-    if(document.getElementById("shipping-address").checked === true){
-        document.getElementById("other-card-address").style.display = "none"  
-        let content =  `${document.getElementById("address").value}, ${document.getElementById("aptFloorOther").value},  ${document.getElementById("city").value}, ${document.getElementById("state").value}, ${document.getElementById('zipCode').value}`;
-        
-        document.getElementById("card-address-value").textContent = content
-    }else{
-        document.getElementById("card-address-value").textContent = ""
-    }
+    document.getElementById("card-address-value").textContent = content;
+  } else {
+    document.getElementById("card-address-value").textContent = "";
+  }
 }
 
-document.getElementById("other-address").addEventListener("change",(event)=>{
-    if(event.target.checked === true){
-        document.getElementById("other-card-address").style.display = "flex"
-        document.getElementById("card-address-value").textContent = ""
-    }else{
-        document.getElementById("other-card-address").style.display = "none"
-    }
+document.getElementById("other-address").addEventListener("change", (event) => {
+  if (event.target.checked === true) {
+    document.getElementById("other-card-address").style.display = "flex";
+    document.getElementById("card-address-value").textContent = "";
+  } else {
+    document.getElementById("other-card-address").style.display = "none";
+  }
 });
 
-document.getElementById("shipping-address").addEventListener("change",cardAddress)
-document.querySelector(".enext-btn").addEventListener("click",NextButtonClick)
+document
+  .getElementById("shipping-address")
+  .addEventListener("change", cardAddress);
+document.querySelector(".enext-btn").addEventListener("click", NextButtonClick);
+
+const diamondJozu = (id) => {
+  const element = document.getElementById(id);
+  if (!element.nextElementSibling) {
+    const spanError = document.createElement("span");
+    spanError.className = "error-message";
+    spanError.innerText = `Please Fill the required field: ${element.previousElementSibling.innerText}`;
+    element.insertAdjacentElement("afterend", spanError);
+  } else {
+    element.nextElementSibling.innerText = `Please Fill the required field: ${element.previousElementSibling.innerText}`;
+  }
+};
 
 function handleNextButtonClick(e) {
-    const next_section =
-        e.target.parentElement.parentElement.parentElement.nextElementSibling;
+  const next_section =
+    e.target.parentElement.parentElement.parentElement.nextElementSibling;
 
-    try {
-        if (
-        next_section.firstElementChild.textContent === "Contact"
-        ) {
-        checkPersonalInfoSection();
-        } else if (
-        next_section.firstElementChild.textContent === "Disclosure"
-        ) {
-        checkContactInfoSection();
-        } else if (
-        next_section.firstElementChild.textContent ===
-        "Payment"
-        ) {
-            cardAddress();
-            checkDisclosure();
-        }
-    } catch (error) {
-        alert(error.message);
-        return;
-    }   
-
-    next_section.style.display = "block";
-
-    const nav_buttons = document.getElementsByClassName("step-trigger");
-
-
-    e.target.parentElement.parentElement.parentElement.style.display =
-        "none";
-
-    if (next_section.firstElementChild.textContent === "Processing" || next_section.firstElementChild.textContent === "Review") {
-        const submit_btn = document.getElementById("submit-button");
-        submit_btn.style.display = "block";
+  try {
+    if (next_section.firstElementChild.textContent === "Contact") {
+      checkPersonalInfoSection();
+    } else if (next_section.firstElementChild.textContent === "Disclosure") {
+      checkContactInfoSection();
+    } else if (next_section.firstElementChild.textContent === "Payment") {
+      cardAddress();
+      checkDisclosure();
     }
+  } catch (error) {
+    diamondJozu(error.message);
+    return;
+  }
 
-    for (let btn of nav_buttons) {
-        if (
-        btn.children[1].textContent ==
-        next_section.firstElementChild.textContent
-        ) {
-            if(window.screen.width < 500){
-                btn.previousElementSibling.previousElementSibling.style.display ="none"
-                btn.style.display = "block"
-            }
-        btn.previousElementSibling.style.color = "#7CCD44";
-        btn.getElementsByTagName("svg")[0].setAttribute("fill", "#7CCD44");
-        btn.children[1].style.color = "#7CCD44";
-        }
-        btn.addEventListener("click",handleStep)
+  const reachMethodInput = document.querySelectorAll(
+    'input[name="reachMethod"]'
+  );
+  const residentialAddress = document.querySelectorAll(
+    'input[name="residentialAddress"]'
+  );
+
+  const removeSpanText = () => {
+    document.getElementById("reachMethod").nextElementSibling.innerText = "";
+  };
+
+  const removeSpanText2 = () => {
+    document.getElementById("residentialAddress").nextElementSibling.innerText =
+      "";
+  };
+
+  for (let reach of reachMethodInput) {
+    reach.addEventListener("click", removeSpanText);
+  }
+  for (let reach of residentialAddress) {
+    reach.addEventListener("click", removeSpanText2);
+  }
+
+  next_section.style.display = "block";
+
+  const nav_buttons = document.getElementsByClassName("step-trigger");
+
+  e.target.parentElement.parentElement.parentElement.style.display = "none";
+
+  if (
+    next_section.firstElementChild.textContent === "Processing" ||
+    next_section.firstElementChild.textContent === "Review"
+  ) {
+    const submit_btn = document.getElementById("submit-button");
+    submit_btn.style.display = "block";
+  }
+
+  for (let btn of nav_buttons) {
+    if (
+      btn.children[1].textContent == next_section.firstElementChild.textContent
+    ) {
+      if (window.screen.width < 500) {
+        btn.previousElementSibling.previousElementSibling.style.display =
+          "none";
+        btn.style.display = "block";
+      }
+      btn.previousElementSibling.style.color = "#7CCD44";
+      btn.getElementsByTagName("svg")[0].setAttribute("fill", "#7CCD44");
+      btn.children[1].style.color = "#7CCD44";
     }
+    btn.addEventListener("click", handleStep);
+  }
 }
 
-
-
 const previous_btn = document.getElementsByClassName("previous-btn");
-document.getElementsByClassName("add-previous-btn")[0].addEventListener("click",(event)=>{
-    const divContainer = document.getElementsByClassName("eligibiliy-form")[0].firstElementChild
-    divContainer.style.display = "block"
-    const container = event.target.parentElement.nextElementSibling
-    container.style.display = "flex"
-    event.target.parentElement.style.display = "None"
+document
+  .getElementsByClassName("add-previous-btn")[0]
+  .addEventListener("click", (event) => {
+    const submit_btn = document.getElementById("no");
+    submit_btn.click();
+    document.getElementById("elgibility-error").innerText = "";
+    const divContainer =
+      document.getElementsByClassName("eligibiliy-form")[0].firstElementChild;
+    divContainer.style.display = "block";
+    const container = event.target.parentElement.nextElementSibling;
+    container.style.display = "flex";
+    event.target.parentElement.style.display = "None";
     var selectedEligibility = document.querySelector(
-        'input[name="eligibility"]:checked'
-    )
-    const dummyValue = divContainer.querySelector("label:nth-child(3)>input")
-    dummyValue.click()
-    selectedEligibility.checked = true
-    selectedEligibility.parentElement.style["border-color"] = "#6786C2"
-    dummyValue.parentElement.style["border-color"] = "rgb(227, 227, 227)"
-})
+      'input[name="eligibility"]:checked'
+    );
+    const dummyValue = divContainer.querySelector("label:nth-child(3)>input");
+    dummyValue.click();
+    selectedEligibility.checked = true;
+    selectedEligibility.parentElement.style["border-color"] = "#6786C2";
+    dummyValue.parentElement.style["border-color"] = "rgb(227, 227, 227)";
+  });
 
 for (let btn of previous_btn) {
-btn.addEventListener("click", handlePreviousButtonClick);
+  btn.addEventListener("click", handlePreviousButtonClick);
 }
 
 function handlePreviousButtonClick(e) {
-    const prevoius_section =
-        e.target.parentElement.parentElement.parentElement
-        .previousElementSibling;
+  const prevoius_section =
+    e.target.parentElement.parentElement.parentElement.previousElementSibling;
 
-    const nav_buttons = document.getElementsByClassName("step-trigger");
-    prevoius_section.style.display = "block";
+  const nav_buttons = document.getElementsByClassName("step-trigger");
+  prevoius_section.style.display = "block";
 
-    e.target.parentElement.parentElement.parentElement.style.display =
-        "none";
+  e.target.parentElement.parentElement.parentElement.style.display = "none";
 
+  if (prevoius_section.firstElementChild.textContent === "Review") {
+    const submit_btn = document.getElementById("submit-button");
+    submit_btn.style.display = "none";
+  }
+
+  for (let btn of nav_buttons) {
     if (
-        prevoius_section.firstElementChild.textContent ===
-        "Review"
+      btn.children[1].textContent ==
+      e.target.parentElement.parentElement.parentElement.firstElementChild
+        .textContent
     ) {
-        const submit_btn = document.getElementById("submit-button");
-        submit_btn.style.display = "none";
+      if (window.screen.width < 500) {
+        btn.previousElementSibling.previousElementSibling.style.display =
+          "block";
+        btn.style.display = "none";
+      }
+      if (btn.children[1].textContent !== "Start") {
+        btn.previousElementSibling.style.color = "#888";
+        btn.getElementsByTagName("svg")[0].setAttribute("fill", "#888");
+        btn.children[1].style.color = "#888";
+      }
     }
-
-    for (let btn of nav_buttons) {
-        if (
-        btn.children[1].textContent ==
-        e.target.parentElement.parentElement.parentElement.firstElementChild
-            .textContent
-        ) {
-            if(window.screen.width < 500){
-                btn.previousElementSibling.previousElementSibling.style.display ="block"
-                btn.style.display = "none"
-            }
-            if (btn.children[1].textContent !== "Start") {
-                btn.previousElementSibling.style.color = "#888";
-                btn.getElementsByTagName("svg")[0].setAttribute("fill", "#888");
-                btn.children[1].style.color = "#888";
-            }
-        }
-    }
+  }
 }
 
 function handleStep() {
@@ -182,8 +220,8 @@ function handleStep() {
   const sect = document.getElementsByClassName("section");
   const nav_buttons = document.getElementsByClassName("step-trigger");
 
-  if(button.getElementsByTagName("svg")[0].getAttribute("fill") === "#888"){
-    return
+  if (button.getElementsByTagName("svg")[0].getAttribute("fill") === "#888") {
+    return;
   }
 
   for (let element of sect) {
@@ -192,8 +230,7 @@ function handleStep() {
     }
 
     if (
-      element.firstElementChild.textContent ===
-      button.children[1].textContent
+      element.firstElementChild.textContent === button.children[1].textContent
     ) {
       element.style.display = "block";
     } else if (element.firstElementChild.tagName === "BUTTON") {
@@ -208,9 +245,7 @@ function handleStep() {
     if (flag) {
       if (btn.children[1].textContent !== "Start") {
         btn.previousElementSibling.style.color = "#7CCD44";
-        btn
-          .getElementsByTagName("svg")[0]
-          .setAttribute("fill", "#7CCD44");
+        btn.getElementsByTagName("svg")[0].setAttribute("fill", "#7CCD44");
         btn.children[1].style.color = "#7CCD44";
       }
       if (btn === button) {
@@ -229,65 +264,62 @@ review_btn[0].addEventListener("click", updateReviewSection);
 // Function to update the review section with input values
 
 function updateReviewSection(event) {
-    try {
-        checkPaymentSection();
-        checkDisclosure();
-    } catch (error) {
-        alert(error.message);
-        return;
-    }
+  try {
+    checkPaymentSection();
+    checkDisclosure();
+  } catch (error) {
+    diamondJozu(error.message);
+    return;
+  }
 
-    try {
-        const lastName = document.getElementById("lastName").value;
+  try {
+    const lastName = document.getElementById("lastName").value;
 
-        document.getElementById("reviewLastName").textContent = lastName;
+    document.getElementById("reviewLastName").textContent = lastName;
 
-        const secondLastName =
-        document.getElementById("secondLastName").value;
+    const secondLastName = document.getElementById("secondLastName").value;
 
-        document.getElementById("reviewSecondLastName").textContent =
-        secondLastName;
+    document.getElementById("reviewSecondLastName").textContent =
+      secondLastName;
 
-        const middleName = document.getElementById("middleName").value;
+    const middleName = document.getElementById("middleName").value;
 
-        document.getElementById("reviewMiddleName").textContent = middleName;
+    document.getElementById("reviewMiddleName").textContent = middleName;
 
-        const address = document.getElementById("address").value;
+    const address = document.getElementById("address").value;
 
-        document.getElementById("reviewAddress").textContent = address;
+    document.getElementById("reviewAddress").textContent = address;
 
-        const aptFloorOther = document.getElementById("aptFloorOther").value;
+    const aptFloorOther = document.getElementById("aptFloorOther").value;
 
-        document.getElementById("reviewAptFloorOther").textContent =
-        aptFloorOther;
+    document.getElementById("reviewAptFloorOther").textContent = aptFloorOther;
 
-        const city = document.getElementById("city").value;
+    const city = document.getElementById("city").value;
 
-        document.getElementById("reviewCity").textContent = city;
+    document.getElementById("reviewCity").textContent = city;
 
-        const zipCode = document.getElementById("zipCode").value;
+    const zipCode = document.getElementById("zipCode").value;
 
-        document.getElementById("reviewZipCode").textContent = zipCode;
+    document.getElementById("reviewZipCode").textContent = zipCode;
 
-        const state = document.getElementById("state").value;
+    const state = document.getElementById("state").value;
 
-        document.getElementById("reviewState").textContent = state;
-} catch (error) {
+    document.getElementById("reviewState").textContent = state;
+  } catch (error) {
     console.log(error.message);
-}
+  }
 
+  // If no errors occur, call the next function
+  handleNextButtonClick(event);
+  const popup = document.getElementById("processing-popup");
+  popup.style.display = "flex";
 
-// If no errors occur, call the next function
-handleNextButtonClick(event);
-const popup = document.getElementById("processing-popup");
-    popup.style.display = "flex";
+  // Optionally, you can submit the form here if needed
 
-    // Optionally, you can submit the form here if needed
-
-    // Close the popup after a certain time (e.g., 3 seconds)
-    setTimeout(() => {
+  // Close the popup after a certain time (e.g., 3 seconds)
+  setTimeout(() => {
     popup.style.display = "none";
-    }, 3000);
+  }, 3000);
 }
 
-export {handleNextButtonClick}
+export { handleNextButtonClick };
